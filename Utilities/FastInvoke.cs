8d43b5fe2b94a532492aae0532078955bfeb3a6a
sebas77 <ssense@tiscali.it>
2018-08-06 10:47:25 +0100

    forgot to commit the new ActionRef variant

--- Utilities/FastInvoke.cs
+++ Utilities/FastInvoke.cs
@@ -150,5 +150,6 @@ namespace Svelto.Utilities
     
     public delegate void ActionCast<T>(ref T target, object value);
     public delegate void ActionRef<T, W>(ref T target, ref W value);
+    public delegate void ActionRef<T, T1, W>(ref T target1, ref T1 target2, ref W value);
     public delegate void ActionRef<T>(ref T target);
 }
