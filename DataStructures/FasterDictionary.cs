a36cfcd241d32f4de7051e07b3d009395c2f6ce3
sebas77 <ssense@tiscali.it>
2020-02-18 19:01:55 +0000

    remove ref params from ThreadSafeDictionary Add CopyValuesTo(array) in FasterDictionary and ThreadSafeDictionary

--- DataStructures/FasterDictionary.cs
+++ DataStructures/FasterDictionary.cs
@@ -95,6 +95,7 @@ namespace Svelto.DataStructures
         }
         
         public bool Contains(KeyValuePair<TKey, TValue> item) { throw new NotImplementedException(); }
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex){ throw new NotImplementedException(); }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public bool ContainsKey(TKey key)
@@ -107,7 +108,10 @@ namespace Svelto.DataStructures
             return false;
         }
 
-        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex) { throw new NotImplementedException(); }
+        public void CopyValuesTo(TValue[] tasks)
+        {
+            Array.Copy(valuesArray, tasks, Count);
+        }
 
         IEnumerator<KeyValuePair<TKey, TValue>> IEnumerable<KeyValuePair<TKey, TValue>>.GetEnumerator()
         {
