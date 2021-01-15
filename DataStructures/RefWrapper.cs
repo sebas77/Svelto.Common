dfa8568404376ba81161cf1fa812987eaada5a69
GitHub <noreply@github.com>
2021-01-15 18:17:56 +0000

    UPM package version 3.1.3

--- DataStructures/RefWrapper.cs
+++ DataStructures/RefWrapper.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Svelto.DataStructures
 {
@@ -31,22 +32,47 @@ namespace Svelto.DataStructures
         readonly int  _hashCode;
     }
     
-    public readonly struct RefWrapper<T>: IEquatable<RefWrapper<T>> where T:class
+    public readonly struct RefWrapper<T>: IEquatable<RefWrapper<T>>, IEqualityComparer<RefWrapper<T>>, IEquatable<T>, IEqualityComparer<T> where T:class
     {
         public RefWrapper(T obj)
         {
-            _value = obj;
+            _value    = obj;
             _hashCode = _value.GetHashCode();
         }
 
         public bool Equals(RefWrapper<T> other)
         {
-            return _value == other._value;
+            return _value.Equals(other._value);
         }
-        
+
+        public bool Equals(T other)
+        {
+            return _value.Equals(other);
+        }
+
         public override int GetHashCode()
         {
             return _hashCode;
+        }
+        
+        public bool Equals(RefWrapper<T> x, RefWrapper<T> y)
+        {
+            return EqualityComparer<T>.Default.Equals(x._value, y._value);
+        }
+
+        public int GetHashCode(RefWrapper<T> obj)
+        {
+            return obj._hashCode;
+        }
+
+        public bool Equals(T x, T y)
+        {
+            return EqualityComparer<T>.Default.Equals(x, y);
+        }
+
+        public int GetHashCode(T obj)
+        {
+            return obj.GetHashCode();
         }
         
         public static implicit operator T(RefWrapper<T> t) => t._value;
